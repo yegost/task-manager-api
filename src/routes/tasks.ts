171bffdfc678e10eae3ca4de5108e7d3fa344320
yegost <caza.alexandru@yahoo.com>
2026-05-23 22:14:05 +0100

    add get by id route

--- src/routes/tasks.ts
+++ src/routes/tasks.ts
@@ -33,7 +33,22 @@ router.post('/', async (req: Request & {
         res.status(201).json(rows[0])
 
     } catch (error) {
-        console.log(error)
+        res.status(500).json({ error: "Internal server error" })
+    }
+})
+
+router.get('/:id', async (req, res) => {
+    const { id } = req.params
+
+    try {
+        const { rows } = await pool.query("SELECT * FROM tasks WHERE id = $1", [id])
+
+        if (rows[0] === undefined) {
+            return res.status(404).json({ error: "No task with that id"})
+        }
+
+        res.status(200).json(rows[0])
+    } catch (error) {
         res.status(500).json({ error: "Internal server error" })
     }
 })
